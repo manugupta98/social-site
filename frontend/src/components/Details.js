380205f61b74f246524a8bb047cf850dfdcc3172
ansh1482 <46290844+ansh1482@users.noreply.github.com>
2020-08-06 17:30:23 +0530

    Update Details.js

--- frontend/src/components/Details.js
+++ frontend/src/components/Details.js
@@ -1,14 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import DatePicker from "./DatePicker"
+import TimePicker from "./TimePicker"
+
 
 export default function Detail() {
     return (
       <View>
-        <TextInput style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
-            placeholder="From"></TextInput>
-        <TextInput style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
-            placeholder="to"></TextInput>
+        <View>
+          <Text>Destination</Text>
+          <TextInput 
+            style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+            placeholder="From">   
+          </TextInput>
+          <TextInput 
+            style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+            placeholder="To">  
+          </TextInput>
+        </View>
+        <View>
+          <Text>Date</Text>
+          <DatePicker />
+          <Text>Time</Text>
+          <Text>From</Text>
+          <TimePicker />
+          <Text>To</Text>
+          <TimePicker />
+        </View>
+        <Button 
+          title="Post it"
+        />
+        <Button 
+          title="Find a suitable group"
+        />
       </View>
     );
   }
